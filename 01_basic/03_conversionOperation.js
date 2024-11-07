f28d2eec8cd6b169d7c3c3cd1b0109fd2f9f1db5
Anku kumar <144608618+ankuis@users.noreply.github.com>
2024-11-07 15:07:31 +0000

    dataTypeConversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+//let score = "33abc";
+//let score = null ;
+//let score = undefined;
+//let score = true;
+let score = "Anku";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" =>33
+// "33abc" =>NaN
+// true => 1, false=>0
+
+//let isLoggenIn = 1;
+//let isLoggenIn = "";
+let isLoggenIn = "Anku";
+
+let booleanIsLoggedIn = Boolean(isLoggenIn);
+//console.log(booleanIsLoggedIn);
+/* 
+    1 => true, 0 =>false
+    " " => false, "Anku" => true
+*/
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
