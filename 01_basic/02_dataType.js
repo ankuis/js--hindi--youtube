089ef21c65cf1ed1d0e56c9a097ba01dad1ce266
Anku kumar <144608618+ankuis@users.noreply.github.com>
2024-11-07 07:07:35 +0000

    dataType done

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict";   //treat all js code as newer version
+
+//alert(3 + 3);  // we're using nodejs not browser
+
+let name = "Anku";
+let age = 22;
+let isLoggenIn = false;
+
+//int => 2 the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+//
+
+//object
+
+console.log(typeof age);  //number
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
