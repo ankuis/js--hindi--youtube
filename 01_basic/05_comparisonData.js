f6bd5fa2bd2f5fe04163406190db3e3aef120c1d
Anku kumar <144608618+ankuis@users.noreply.github.com>
2024-11-08 17:42:45 +0000

    comparisonDataTypes

---
+++ 01_basic/05_comparisonData.js
@@ -0,0 +1,17 @@
+// comparison of data types
+//console.log(2>1);
+//console.log(2>=1);
+
+//console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0)
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//  ===
+console.log("2" === 2);
