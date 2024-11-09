dc196cecc876f86d5e1c3e52f18b83a7e6d12e83
Anku kumar <144608618+ankuis@users.noreply.github.com>
2024-11-09 18:12:33 +0000

    string

---
+++ 01_basic/08_string.js
@@ -0,0 +1,20 @@
+//string
+const name = "Anku "
+const repoCount = 5
+
+//console.log(name + repoCount + " value")  // old method to write this now learn modern below
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+// above we used back ticks and called string interpolation
+
+const gameName = new String("ankuac")
+
+//console.log(gameName[0])
+//console.log(gameName.__proto__)
+
+//console.log(gameName.length)
+//console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(5))
+console.log(gameName.indexOf('a'))
